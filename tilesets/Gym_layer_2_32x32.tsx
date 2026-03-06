9d4adf66272bc56c7fc3a76c50e2b6213e6ab688
Jhonatan Lucco <jholucco1303@gmail.com>
2026-03-06 10:35:36 -0300

    feat:  office ethosx

---
+++ tilesets/Gym_layer_2_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Gym_layer_2_32x32" tilewidth="32" tileheight="32" tilecount="285" columns="19">
+ <image source="../../WA_Assets/moderninteriors-win/6_Home_Designs/Gym_Designs/32x32/Gym_layer_2_32x32.png" width="608" height="480"/>
+</tileset>
---
+++ tilesets/WA_Assets/Modern_Office_Revamped_v1.2/Room_Builder_Office_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Room_Builder_Office_32x32" tilewidth="32" tileheight="32" tilecount="224" columns="16">
+ <image source="../../WA_Assets/Modern_Office_Revamped_v1.2/1_Room_Builder_Office/Room_Builder_Office_32x32.png" width="512" height="448"/>
+</tileset>
